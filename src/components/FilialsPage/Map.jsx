9a1d0b732e269778428cf95d7e0e485509d395a5
Resterr1337 <Franklion1chance@gmail.com>
2024-09-25 11:19:29 +0500

    Fillial Map Update

--- src/components/FilialsPage/Map.jsx
+++ src/components/FilialsPage/Map.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { useMap, Marker } from "react-leaflet";
+import {Icon} from 'leaflet'
+import 'leaflet/dist/leaflet.css';
 
 
 const FilialMarker = () => {
+    const markerIcon = new Icon({
+        iconUrl:"https://pngimg.com/uploads/gps/gps_PNG38.png",
+        iconSize:[25, 40],
+        iconAnchor:[12.5, 45]
+    })
     const [latIng , setLatIng] = useState([12, 12])
 
     const map = useMap()
     map.on("click", (e) => {
         setLatIng([e.latlng.lat , e.latlng.lng])
     })
-	return <Marker position={latIng}/>;
+	return <Marker icon={markerIcon} position={latIng}/>;
 };
 
 export { FilialMarker };
